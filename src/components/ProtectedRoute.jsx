de3073fa6d2b629babdcfabe73d0edf79596550f
Shubham <sv9170773305@gmail.com>
2026-06-28 23:04:01 +0530

    Fix protected route loading

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,28 @@
-import { useUser } from '@clerk/clerk-react'
-import React from 'react'
-import { Navigate } from 'react-router-dom';
+// import { useUser } from '@clerk/clerk-react'
+// import React from 'react'
+// import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({children}) => {
-  const {user} = useUser();
-  return (
-    <div>
-      {user ? children : <Navigate to='/'/>}
-    </div>
-  )
-}
+// const ProtectedRoute = ({children}) => {
+//   const {user} = useUser();
+//   return (
+//     <div>
+//       {user ? children : <Navigate to='/'/>}
+//     </div>
+//   )
+// }
 
-export default ProtectedRoute
+// export default ProtectedRoute
+
+
+import { useAuth } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return isSignedIn ? children : <Navigate to="/sign-in" replace />;
+};
+
+export default ProtectedRoute;
